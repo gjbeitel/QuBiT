e247544b73b9eb69b8d8810777cb76a1cd160175
Ran Yang <r.yang613@gmail.com>
2018-09-19 15:52:13 -0700

    180919 update
    Fixed code in module 6, renamed directories, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,105 @@
-# QuBiT
-Quantitative Tool for Biological Tubes (QuBiT)
+# QuBiT - Quantification Tool for Biological Tubes
+
+
+Overview:
+We developed QuBiT, or Quantification tool for Biological Tubes, to measure thin biological tubes accurately and reproducibly in 3D. QuBiT is a series of Matlab scripts to be used with segmented confocal stacks, such as the output of Ilastik (http://ilastik.org). 
+
+Minimum system requirements:
+
+Hardware:
+RAM: 8GB
+CPU: 2.5GHz
+OS: MacOSX 10.5 (Leopard)
+
+Software:
+Matlab 2014b
+  Matlab code base for multi-stencil fast marching (MSFM) (see: Hassouna and Farag 2007)
+
+
+
+
+This is a list of all Matlab script used in QuBiT, in roughly sequential order of the described image analysis method.
+
+Module 1: Import tubes and skeletonize
+s1_import_tubes
+ 
+ This module imports the segmentation data for the tube (e.g. the *.h5 segmentation file exported from Ilastik) and skeletonizes them using MSFM. You can select multiple files to process overnight. The resulting datasets are split and stored in individual structures which will contain all post-processing data for each dataset.
+
+ 
+ 
+Module 2: Display tubes
+s2_display_tubes
+ 
+ This module displays the segmented and skeletonized tubes. This is a check for proper skeletonization by visualizing the result in 3D. Segments of the skeleton are individually colored and indexed. Use the indices to specify the ROI in the next module.
+ 
+ 
+Module 3: Specify tube ROI
+s3_tube_ROI
+ 
+ This module is required to specify the tube regions of interest. Enter the segment numbers sequentially as they appear in the figure created in module 2.
+ 
+ 
+Module 4: Calculate tube and segment length
+s4_calc_tube_length
+ 
+ This module calculates length of the ROI using the skeleton and gathers information on coordinates of individual tube segments, which will be used in later calculations for e.g. normalizing segment data.
+ 
+ 
+Module 5: Calculate tube cross-sections
+s5_calc_cross_section
+ 
+ This module calculates individual cross-sections, defined as the plane of the tube that is orthogonal to the local skeleton tangent vector. A figure containing the shapes of the successive cross-sections as well as their areas and circumferences can be displayed.
+ 
+ 
+ 
+Module 6: Import surface data and mask them on the tube surface
+s6_handle_junctions
+ 
+ This module allows you to input an image stack of cell junctions or any tube surface data from the specified folder. It will then invert and project this image onto a mask of the tube with parameters specified by dilation_radius and dilation_value. (Default values result in 1 pixel-wide mask directly above the surface of the tube.)
+
+This module can display the resulting masked data, overlaying the tube image and skeleton. This is done to ensure that the masked data appear acceptable for analysis and that the three components (tube skeleton, tube image, cell image) are all correctly aligned.
+ 
+ 
+Module 7: Calculate surface object data
+s7_calc_cells
+
+ 
+ This module calculates properties of interest of cells or surface objects defined in the previous step. Properties include apical cell size, aspect ratio, and orientation relative to the tube axis.
+ 
+ 
+Module 8: Normalize (and filter) surface object data
+s8_normalize_cell_segments
+
+ This module normalizes the data to cell segments specified in module 3 and applies user-created filters. 
+ 
+
+Module 9: Unroll tube (create 2D surface plot)
+s9_unroll_tube
+ 
+ This module indexes the tube surface as if it were a thin layer and plots the data as a flat 2D image. This is particularly useful to visualize patterns on surface data. Since unrolling a non-cylindrical tube will deform it, direct calculations on the 2D image should be avoided.
+
+
+Module: Reroll tube
+
+ This module plots the 2D surface plot data from module 9 on a thin conical surface in 3D.
+
+
+
+Supporting scripts
+
+These scripts were used for debugging purposes and/or generating figures specifically for the QuBiT paper and may not be directly applicable to other projects. They are included for reproducibility and as potential starting points for other analyses using QuBiT.
+
+s7x_plot_cells
+s7y_bulk_reapply_filters
+s8_view_segments
+s9a_refine_tube (resulting data not used in paper)
+s10a_set_colors
+s10b1_aggregate_tube_data
+s10b2_plot_tube_data
+s10c1_aggregate_cell_data
+s10c2_plot_cell_data
+s10d2_plot_individual_celldata
+s10e1_aggregate_2d_data
+s10e2_plot_2d_data
+s10f1_aggregate_reroll_data
+s10f2_plot_reroll_data
--- scripts/s6_handle_junctions.m
+++ scripts/s6_handle_junctions.m
@@ -1,59 +1,54 @@
-% function allFiles = s6_handle_junctions_f(allFiles,directory,dilation_radius,dilation_factor,base_int,preview,save_dir)
-
 %% Import cell junction data using directory of image stack
 
 % user defined parameters:
 if exist('dilation_radius','var') == 0
     dilation_radius = 1;
 end
-if exist('dilation_factor','var') == 0
-    dilation_factor = 1;
+if exist('dilation_value','var') == 0
+    dilation_value = 1;
 end
-if 2*dilation_radius < dilation_factor
+if 2*dilation_radius < dilation_value
     error('Surface parameters invalid. Check dilation_radius and dilation_value');
 end
 
 
-
-fprintf('Import junctions stack directory...\n');
+% case 3
+fprintf('\nImport junctions stack directory...\n');
 folder_name = uigetdir;
 D = dir(folder_name);
 D2 = D(not([D.isdir]));
-imNames = {D2.name}';
 DirSize = length(D2);
 
-I = imread(imNames{1});
-sequence = zeros([size(I) DirSize],class(I));
-sequence(:,:,1) = I;
-
-for p = 2:DirSize
-    sequence(:,:,p) = imread(imNames{p});
-end
-%% Debug only
+% 
 % try
 %     test_file = sprintf('%s/imj0000.tif',folder_name);
 %     test = imread(test_file);
 %     sequence_pattern = 'imj0';
 %     sequence_begin = 0;
 % catch
-%
-%
+% 
+% 
 % sequence_pattern = input('Input image sequence pattern.\nFor example, "T00001C01Z" for raw images or "imj0" (no quotes).\n','s');
 % sequence_begin = input('Input stack start value. This is usually 0 or 1.\n');
-%
-%
+% 
+% 
 % test_file = sprintf('%s/%s%03i.tif',folder_name,sequence_pattern,sequence_begin);
 % try
 %     test = imread(test_file);
 % catch
 %     error('Unable to import image sequences.');
 % end
-%
+% 
 % end
-%
-%
-%
-%
+
+fprintf('\nWorking...\n');
+I = imread([D2(1).folder,'/',D2(1).name]);
+lm12 = uint8(zeros(size(I,1),size(I,2),DirSize));
+
+for i = 1:DirSize
+    lm12(:,:,i) = imread([D2(i).folder,'/',D2(i).name]);
+end
+
 % for ii = sequence_begin:DirSize+sequence_begin-1
 %     file_name = sprintf('%s/%s%03i.tif',folder_name,sequence_pattern,ii);
 %     try
@@ -62,15 +57,17 @@ end
 %         warning('Directory size exceeds image stack size. Usually this is due to hidden files in the directory.');
 %     end
 % end
-%
-% % while range(lm12(:,:,end)) == 0
-% %     lm12(:,:,end) = [];
-% % end
-%
-% base_int = input('\nInput base intensity threshold.\n You can also enter a decimal for a percentage threshold, or\n -1 to auto-threshold by the mean intensity.\n');
-%
+
+% while range(lm12(:,:,end)) == 0
+%     lm12(:,:,end) = [];
+% end
+
+fprintf('\nImage sequence imported successfully.');
+
+base_int = input('\nQuick intensity threshold segmentation:\n');
+
 if base_int == -1
-    base_int = mean(lm12(:));
+    base_int = mean(lm12(lm12~=0));
 elseif (0 < base_int) && (base_int < 1)
     percent_threshold = base_int*numel(lm12);
     [~,idx] = sort(lm12(:), 'descend');
@@ -78,63 +75,62 @@ elseif (0 < base_int) && (base_int < 1)
     base_int = min(values);
 end
 
-%%
-lm12 = uint8(zeros(size(sequence,1),size(sequence,2),DirSize-1));
-fprintf('Image sequence imported successfully.\n');
 
 lm13 = lm12;
 lm13(lm13 <= base_int) = 0;
 lm13(lm13 ~= 0) = 1;
 
-
 %% Create cells through inversion of cell boundaries on apical surface and export
 
 fprintf('Mapping cells...\n');
-FileList = fieldnames(allFiles);
+varname = fieldnames(allFiles);
+fileNo = 1;
 
-try xscale = allFiles.(FileList{1}).xscale;
-catch
-    % warning('Image X scaling info not found in allFiles. Defaulted to 1.');
-    xscale = 1;
-end
-try yscale = allFiles.(FileList{1}).yscale;
-catch
-    % warning('Image Y scaling info not found in allFiles. Defaulted to 1.');
-    yscale = 1;
-end
-try zscale = allFiles.(FileList{1}).zscale;
-catch
-    % warning('Image Z scaling info not found in allFiles. Defaulted to 1.');
-    zscale = 1;
-end
+% try xscale = allFiles.(varname{fileNo}).xscale;
+% catch
+%     warning('Image X scaling info not found in allFiles. Defaulted to 1.');
+%     xscale = 1;
+% end
+% try yscale = allFiles.(varname{fileNo}).yscale;
+% catch
+%     warning('Image Y scaling info not found in allFiles. Defaulted to 1.');
+%     yscale = 1;
+% end
+% try zscale = allFiles.(varname{fileNo}).zscale;
+% catch
+%     warning('Image Z scaling info not found in allFiles. Defaulted to 1.');
+%     zscale = 1;
+% end
 
-jp = lm13;
-
-tube2 = allFiles.(FileList{1}).h5bwfilled;
-tube = permute(tube2, [1 3 2]); % switch XY
-expdims = zeros(ceil(size(tube,1)*zscale/xscale),ceil(size(tube,2)*zscale/yscale),size(tube,3));
-cubetube = expdims; % scale to 1:1:1 based on zscale, assuming xscale=yscale
-cubejuns = expdims;
-
-if (xscale ~= 1) && (yscale ~= 1) && (zscale ~= 1)
-    for i=1:size(tube,3)
-        cubetube(:,:,i) = imresize(tube(:,:,i),zscale/xscale);
-        cubejuns(:,:,i) = imresize(jp(:,:,i),zscale/xscale);
-    end
-else
-    cubetube = tube;
-    cubejuns = jp;
-end
+juns = lm13;
+tube = permute(allFiles.(varname{fileNo}).h5bwfilled, [1 3 2]); % switch XY
+% expdims = zeros(ceil(size(tube,1)*zscale/xscale),ceil(size(tube,2)*zscale/yscale),size(tube,3));
+% cubetube = expdims;
+% cubejuns = expdims;
+% 
+% if (xscale ~= 1) && (yscale ~= 1) && (zscale ~= 1)
+%     for i=1:size(tube,3)
+%         cubetube(:,:,i) = imresize(tube(:,:,i),zscale/xscale);
+%         cubejuns(:,:,i) = imresize(jp(:,:,i),zscale/xscale);
+%     end
+% else
+%   cubetube = tube;
+%   cubejuns = jp;
+% end
 
 
-ctouter = imdilate(cubetube,ones(dilation_radius*2+dilation_factor,dilation_radius*2+dilation_factor,dilation_radius*2+dilation_factor));
-ctinner = imdilate(cubetube,ones(dilation_radius*2-dilation_factor,dilation_radius*2-dilation_factor,dilation_radius*2-dilation_factor));
+ctouter = imdilate(tube,ones(dilation_radius*2+dilation_value,dilation_radius*2+dilation_value,dilation_radius*2+dilation_value));
+ctinner = imdilate(tube,ones(dilation_radius*2-dilation_value,dilation_radius*2-dilation_value,dilation_radius*2-dilation_value));
 ctshell = ctouter - ctinner;
 
 % cubejuns = imdilate(cubejuns,ones(3,5,5));
 % cubejuns = smooth3(cubejuns); % 170217: attempting this with initial smoothing to reduce bg noise and comp time
 
-apicaljunctions = and(ctshell,cubejuns);
+% juns_inv = imcomplement(juns);
+% [sx,sy,sz] = ind2sub(size(ctshell),find(ctshell==1));
+% apicaljunctions = juns_inv(sx,sy,sz);
+
+apicaljunctions = and(ctshell,juns);
 
 [shellx, shelly, shellz] = ind2sub(size(ctshell), find(ctshell == 1));
 for i = 1:size(shellx,1)
@@ -147,13 +143,16 @@ end
 
 bwcellimage = permute(apicaljunctions,[1 3 2]);
 
-allFiles.(FileList{1}).bwcellimage = bwcellimage;
+if exist('FileNo','var') == 0
+    FileNo = 1;
+end
+allFiles.(varname{FileNo}).bwcellimage = bwcellimage;
 
-%
-% fprintf('Preview cell image and tube?\n');
-% preview = input('','s');
 
-if strcmp(preview,'Y')
+fprintf('Preview cell image and tube?\n');
+preview = input('','s');
+
+if preview == 'Y'
     figure;axis equal;axis([-inf inf -inf inf -inf inf]);hold on;
     blah = isosurface(allFiles.(userDefinedallFilesName).h5bwfilled, 0.5);
     patch(blah,'facecolor','red','facealpha',0.3,'edgecolor','none');
@@ -163,19 +162,30 @@ end
 
 %% save
 
-if strcmp(overwrite,'Y')
-    if size(tube2) ~= size(bwcellimage)
-        warning('Dimensions of tube and cell image do not match. Usually this is a dim permutation issue.');
-    end
-    fprintf('Workspace and base file (%s.mat) updated.\n',FileList{1});
-    save(FileList{1},'allFiles','-v7.3','-append');
+fprintf('Save?\n');
+preview = input('','s');
+
+if preview == 'Y'
+allFilesnames = fieldnames(allFiles);
+if exist('userDefinedallFilesName','var') == 0
+    warning('Working file name not found. Unable to auto-save.');
 else
-    fprintf('Workspace updated, but base file was not overwritten.\n');
+    allFilescheck = strcmp(userDefinedallFilesName,allFilesnames);
+    if sum(allFilescheck(:)) == 1
+        if size(tube2) ~= size(bwcellimage)
+            warning('Dimensions of tube and cell image do not match. Usually this is a dim permutation issue.');
+        end
+        fprintf('Saving cell image to allFiles in %s...\n',userDefinedallFilesName);
+        save(userDefinedallFilesName,'allFiles','-v7.3','-append');
+    else
+        warning('The working file name and save file name do not match. Skipping auto-save...');
+    end
+end
 end
 
 return
 
-%% Debug only
+%% old crap
 
 % figure;
 % blah = isosurface(apicaljunctions, 0.3);
@@ -192,15 +202,10 @@ return
 % each jpeg is necessary, i.e. the number 1 is 001.  These
 % jpegs will be allowed to import into ilastik.
 
-if ~isstr(save_dir)
-    mkdir(FileList{1})
-else
-    mkdir(save_dir)
-end
+mkdir(varname{fileNo})
 for ii = 1:size(bwcellimage, 3)
     imwrite(bwcellimage(:,:,ii), sprintf('image%04d.jpg',ii));
-    movefile(sprintf('image%04d.jpg',ii), FileList{1});
+    movefile(sprintf('image%04d.jpg',ii), varname{fileNo});
 end
 
-fprintf('\nStack has successfuly been saved to the folder %s.\nUse Pixel+Object Classification module in Ilastik to map cells. Then return to step 4\n', FileList{1});
-% end
+fprintf('\nStack has successfuly been saved to the folder %s.\nUse Pixel+Object Classification module in Ilastik to map cells. Then return to step 4\n', varname{fileNo});
